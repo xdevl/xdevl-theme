0a3ba9c2dd10b6b40ccdc1a6eaf16b70121d50c3
xdevl <xdevl@users.noreply.github.com>
2015-05-16 19:46:38 +0100

    Added navigation links at the bottom of the post pages

--- index.php
+++ index.php
@@ -2,33 +2,36 @@
 <div id="main">
 	<?php get_sidebar(); ?>
 	<div id="content">
-			<?php if(!have_posts()):
-				get_template_part('post','none') ;
-			elseif(is_single()): ?>
-				<div class="post-featured panel">
-					<?php the_post() ;
-					get_template_part('post') ; ?>
+		<?php if(!have_posts()):
+			get_template_part('post','none') ;
+		elseif(is_single()): ?>
+			<div class="post-featured panel">
+				<?php the_post() ;
+				get_template_part('post') ; ?>
+			</div>
+			<div id="post-navigation">
+				<?php previous_post_link('%link','&laquo; previous post') ;
+				next_post_link('%link','next post &raquo;'); ?>
+			</div>
+		<?php else: ?>
+			<?php if(is_home() && !is_paged()): ?>
+				<div class="orbit-wrapper">
+					<ul data-orbit>
+						<?php for($count=0;$count<2 && have_posts();$count++): ?>
+							<li class="post-featured panel">
+								<?php the_post() ;
+								get_template_part('post') ;?>
+							</li>
+						<?php endfor; ?>
+					</ul>
 				</div>
-			<?php else: ?>
-			
-				<?php if(is_home() && !is_paged()): ?>
-					<div class="orbit-wrapper">
-						<ul data-orbit>
-							<?php for($count=0;$count<2 && have_posts();$count++): ?>
-								<li class="post-featured panel">
-									<?php the_post() ;
-									get_template_part('post') ;?>
-								</li>
-							<?php endfor; ?>
-						</ul>
-					</div>
-				<?php  elseif(is_home() && is_paged()): ?>
-					<h1>Archives</h1>
-				<?php elseif(is_search()): ?>
-					<h1>Search results</h1>
-				<?php elseif(is_category()): ?>
-					<h1><?php echo get_category(get_query_var('cat'))->name ?> posts</h1>
-				<?php endif; ?>
+			<?php  elseif(is_home() && is_paged()): ?>
+				<h1>Archives</h1>
+			<?php elseif(is_search()): ?>
+				<h1>Search results</h1>
+			<?php elseif(is_category()): ?>
+				<h1><?php echo get_category(get_query_var('cat'))->name ?> posts</h1>
+			<?php endif; ?>
 			<div class="post-list">
 				<?php while(have_posts()): the_post(); ?>
 					<div class="post-row">
@@ -50,7 +53,6 @@
 				<?php previous_posts_link('&laquo; newer posts') ;
 				next_posts_link('older posts &raquo;'); ?>
 			</div>
-			
 		<?php endif; ?>
 	</div> <!-- End content -->
 </div> <!-- End main -->
