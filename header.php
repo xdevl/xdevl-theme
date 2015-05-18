ecd47f34b4dfd9e7db608385b9451643a9225e01
xdevl <xdevl@users.noreply.github.com>
2015-05-18 01:17:55 +0100

    Use user defined wordpress site title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
-		<title>XdevL</title>
+		<title><?php bloginfo('name'); ?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body>
