2c377215e2fb15312e00d7b895a69862fcc65cbd
xdevl <xdevl@users.noreply.github.com>
2015-08-31 00:59:47 +0100

    Introduced a new class to style panels with titles

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
-	<div class="categories panel" />
-		<h2 ><?php _e('Categories'); ?></h2>
+	<div class="categories titled-panel" />
+		<h2 class="title"><?php _e('Categories'); ?></h2>
 		<ul class="side-nav">
 			<?php wp_list_categories(array('orderby'=>'name','style'=>'list','show_count'=>0,'hierarchical'=>false,'style'=>'list','title_li'=>'')); ?>
 		</ul>
