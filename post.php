d66afc4bb739767714bf623e8e20e3c59b8d0cf2
xdevl <xdevl@users.noreply.github.com>
2015-09-23 01:34:49 +0100

    Post titles now wrap the post images

--- post.php
+++ post.php
@@ -1,8 +1,10 @@
 <div class="post-featured panel">
-<h2>
+<?php $thumbnail=wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID())); ?>
+<a href="<?php the_permalink(); ?>" rel="bookmark"><img src="<?php echo $thumbnail['0']; ?>" class="wp-post-image" /></a>
+<h2 class="post-title">
 	<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 </h2>
-<hr />
+
 <?php if(!is_page()): ?>
 	<dl>
 		<dt class="category">Category:</dt>
@@ -11,8 +13,7 @@
 		<dd><?php the_time('j F, Y');?></dd>
 	</dl>
 <?php endif; ?>
-<?php $thumbnail=wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID())); ?>
-<a href="<?php the_permalink(); ?>" rel="bookmark"><img src="<?php echo $thumbnail['0']; ?>" class="wp-post-image" /></a>
+
 <?php
 	the_content(__('Read more...')) ;
 ?>
